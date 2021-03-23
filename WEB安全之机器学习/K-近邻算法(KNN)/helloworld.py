4532a9db8db1b53d2bff6554cdbb8ebc70683857
mikewu <xcwu@whu.edu.cn>
2021-03-23 19:44:59 +0800

    Update helloworld.py

--- WEB安全之机器学习/K-近邻算法(KNN)/helloworld.py
+++ WEB安全之机器学习/K-近邻算法(KNN)/helloworld.py
@@ -1,1 +1,19 @@
 
+'''
+@File    :   helloworld.py
+@Time    :   2021/03/23 19:32:15
+@Author  :   mike.wu 
+@Contact :   xcwu@whu.edu.cn
+@github:    https://github.com/wxcon
+'''
+
+from sklearn.neighbors import NearestNeighbors 
+import numpy as np 
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+nbrs = NearestNeighbors(n_neighbors = 2, algorithm='ball_tree').fit(X)
+
+distances, indices = nbrs.kneighbors(X)
+print(distances) 
+print(indices) 
+
+print(nbrs.kneighbors_graph(X).toarray())
