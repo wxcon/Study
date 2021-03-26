9730de8d3d08ce73bc2886cb0e57959d225233af
mikewu <xcwu@whu.edu.cn>
2021-03-26 23:32:55 +0800

    Create csv2html1_ans.py

---
+++ Python3 程序开发指南/第二章/csv2html1_ans.py
@@ -0,0 +1,82 @@
+'''
+@File    :   csv2html1_ans.py
+@Time    :   2021/03/26 17:30:48
+@Author  :   mike.wu 
+@Version :   0.99
+@Contact :   xcwu@whu.edu.cn
+'''
+
+#Description: P4 练习题3 
+
+from xml.sax.saxutils import escape
+
+def main():
+    maxwidth = 100 
+    print_start()
+    count = 0 
+    while True: 
+        try:
+            line = input()
+            if count == 0: 
+                color = "lightgreen"
+            elif count % 2: 
+                color = "white"
+            else: 
+                color = "lightyellow"
+            print_line(line, color, maxwidth)
+            count += 1 
+        except EOFError:
+            break
+    print_end()
+
+def print_start():
+    print("<table border='1'>")
+
+def print_end():
+    print("</table>")
+
+def print_line(line, color, maxwidth):
+    print("<tr bgcolor='{0}'>".format(color))
+    fields = extract_fields(line)
+    for field in fields: 
+        if not field: 
+            print("<td></td>")
+        else:
+            number = field.replace(",","")
+            try: 
+                x = float(number)
+                print("<td align='right'>{0:d}</td>".format(round(x)))
+            except ValueError:
+                field = field.title()
+                field = field.replace("And", "and")
+                if len(field) <= maxwidth: 
+                    field = escape(field)
+                else: 
+                    field = "{0} ...".format(escape(field[:maxwidth]))
+                print("<td>{0}</td>".format(field))
+    print("</tr>")
+
+def extract_fields(line):
+    fields = []
+    field = ""
+    quote = None 
+    for c in line: 
+        if c in "\"":
+            if quote is None: 
+                quote = c 
+            elif quote == c:
+                quote = None 
+            else:
+                field += c 
+            continue 
+        if quote is None and c == ",": 
+            fields.append(field)
+            field = ""
+        else: 
+            field += c 
+    if field: 
+        fields.append(field)
+    return fields 
+
+if __name__ == "__main__":
+    main()
